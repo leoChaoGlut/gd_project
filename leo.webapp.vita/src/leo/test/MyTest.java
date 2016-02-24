7b9f762579ad96b88496313345a85790072b36c6
leoChaoGlut <121559258@qq.com>
2016-02-24 15:55:53 +0800

    upgraded

--- leo.webapp.vita/src/leo/test/MyTest.java
+++ leo.webapp.vita/src/leo/test/MyTest.java
@@ -1,5 +1,9 @@
 package leo.test;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,10 +13,14 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.alibaba.fastjson.JSON;
+
+import leo.bean.Article;
 import leo.bean.Skill;
 import leo.bean.UserSkill;
 import leo.cache.CacheFactory;
 import leo.cache.imp.UserSkillCache;
+import leo.mapper.ArticleMapper;
 import leo.service.IUserService;
 
 public class MyTest {
@@ -58,5 +66,20 @@ public class MyTest {
 		for (UserSkill userSkill : skills) {
 			System.out.println(userSkill.toString());
 		}
+	}
+
+	@SuppressWarnings("resource")
+	@Test
+	public void test5() throws Exception {
+		BufferedReader br = new BufferedReader(new FileReader("c://1.txt"));
+		String line = "";
+		StringBuffer sb = new StringBuffer();
+		while ((line = br.readLine()) != null) {
+			sb.append(line);
+		}
+		List<Article> articles = JSON.parseArray(sb.toString(), Article.class);
+		ArticleMapper articleMapper = ctx.getBean(ArticleMapper.class);
+		int count = articleMapper.insertMore(articles);
+		System.out.println(count);
 	}
 }
